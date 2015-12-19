0c8ab637cc5910ceb91e7d3579c3f9d3d5547f91
deepak.nautiyal <dnautiyal@gmail.com>
2015-12-19 17:47:13 +0530

    Refactoring done

--- app/src/main/java/com/deepaknautiyal/weatherandme/MainActivity.java
+++ app/src/main/java/com/deepaknautiyal/weatherandme/MainActivity.java
@@ -3,14 +3,10 @@ package com.deepaknautiyal.weatherandme;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -46,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
                 +apiKey+"/"+latitude+","+longitude;
 
 
-        if(inNetworkAvailable()) {
+        if(isNetworkAvailable()) {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder().url(forecastUrl).build();
             Call call = client.newCall(request);
@@ -106,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
         return currentWeather;
     }
 
-    private boolean inNetworkAvailable() {
+    private boolean isNetworkAvailable() {
         boolean isAvailable = false;
         ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = manager.getActiveNetworkInfo();

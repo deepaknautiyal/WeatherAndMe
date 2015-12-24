e846e64201404aa5b2f9ecd99249963433b6a929
deepak.nautiyal <dnautiyal@gmail.com>
2015-12-24 20:27:30 +0530

    Real time location look-up added

--- app/src/main/java/com/deepaknautiyal/weatherandme/MainActivity.java
+++ app/src/main/java/com/deepaknautiyal/weatherandme/MainActivity.java
@@ -1,10 +1,17 @@
 package com.deepaknautiyal.weatherandme;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -30,20 +37,33 @@ import java.io.IOException;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
-    private CurrentWeather mCurrentWeather;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
 
-    @Bind(R.id.timeLabel) TextView mTimeLabel;
-    @Bind(R.id.tempratureLabel) TextView mTempratureLabel;
-    @Bind(R.id.humidityValue) TextView mHumidityValue;
-    @Bind(R.id.precipValue) TextView mPrecipValue;
-    @Bind(R.id.summaryLabel) TextView mSummary;
-    @Bind(R.id.iconImageView) ImageView mIconImage;
-    @Bind(R.id.refreshImageView) ImageView mRefreshImage;
-    @Bind(R.id.progressBar) ProgressBar mProgressBar;
+    private CurrentWeather mCurrentWeather;
+    double latitude;
+    double longitude;
+
+    @Bind(R.id.timeLabel)
+    TextView mTimeLabel;
+    @Bind(R.id.tempratureLabel)
+    TextView mTempratureLabel;
+    @Bind(R.id.humidityValue)
+    TextView mHumidityValue;
+    @Bind(R.id.precipValue)
+    TextView mPrecipValue;
+    @Bind(R.id.summaryLabel)
+    TextView mSummary;
+    @Bind(R.id.iconImageView)
+    ImageView mIconImage;
+    @Bind(R.id.refreshImageView)
+    ImageView mRefreshImage;
+    @Bind(R.id.progressBar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +73,34 @@ public class MainActivity extends AppCompatActivity {
 
         mProgressBar.setVisibility(View.INVISIBLE);
 
-        final double latitude = 12.903238;
-        final double longitude = 77.647602;
+        Log.d(TAG, "ProgressBar set to invisible");
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Log.d(TAG, "Location Manager setup.");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            Log.d(TAG,"Inside main IF block");
+            double latitude = 12.903238;
+            double longitude = 77.647602;
+            Log.d(TAG, "Lat =" + latitude+" Long= "+ longitude);
+
+            Toast.makeText(MainActivity.this, R.string.loc_perm_unavailable_msg,
+                    Toast.LENGTH_LONG).show();
+
+            //return;
+        }
+        else{
+            Log.d(TAG, "Inside ELSE block");
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            Log.d(TAG,"real-time location retrieved");
+        }
 
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
@@ -211,5 +257,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d(TAG, "status");
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d(TAG, "enabled");
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d(TAG,"disabled");
+
     }
 }
